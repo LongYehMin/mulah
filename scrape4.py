c0d6237ecdd1b2fe3c1b0864748f43b7ba664be6
LongYehMin <longyehmin@gmail.com>
2023-12-23 13:43:45 +0800

    scrape

--- scrape4.py
+++ scrape4.py
@@ -3,65 +3,66 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 import json
+import calendar
 
-def get_news_sentiment(category, start_date):
+def get_news(category):
     theverge_url = 'https://www.theverge.com/archives/'
-    stop_collecting = False
 
-    # Collecting daily news headlines for the chosen stocks on Business Insider
-    news = {}
+    # Define the range of years and months
+    years = [2022,2023]
+    months = list(range(1, 13))
+
+    # Collecting daily news headlines for the chosen category on The Verge
     all_data = []  # Collect data from all pages
-    page_number = 1
 
-    while not stop_collecting:
-        url = f'{theverge_url}{category}/{start_date.year}/{page_number}'
-        req = Request(url=url, headers={'User-Agent': 'my-app'})
-        response = urlopen(req)
+    for year in years:
+        for month in months:
+            last_day_of_month = calendar.monthrange(year, month)[1]
 
-        soup = BeautifulSoup(response, 'html.parser')
-        
-        articles = soup.find_all('div', class_='c-entry-box--compact__body')
+            for day in range(1, last_day_of_month + 1):
+                url = f'{theverge_url}{category}/{year}/{month}/{day}'
+                req = Request(url=url, headers={'User-Agent': 'my-app'})
 
-        if articles:
-            for article in articles:
-              
-                date_time_str = article.find('div', class_='c-byline')['data-cdata']
-                date_time_json = json.loads(date_time_str)
-                date_time = datetime.utcfromtimestamp(int(date_time_json['timestamp']))
-                
-                headline = article.find('h2', class_='c-entry-box--compact__title').find('a')['data-chorus-optimize-field']
-                link = article.find('h2', class_='c-entry-box--compact__title').find('a')['href']
+                try:
+                    response = urlopen(req)
+                    soup = BeautifulSoup(response, 'html.parser')
 
-                # Check if date is greater than or equal to start_date
-                if date_time < start_date:
-                    # If date is earlier than start_date, set the flag to stop collecting
-                    stop_collecting = True
-                    break
-                                
-                all_data.append([date_time, headline, link])
-                print(date_time, headline, link)
+                    articles = soup.find_all('div', class_='c-entry-box--compact__body')
 
-            # Increment page number for the next iteration
-            page_number += 1
+                    if articles:
+                        for article in articles:
+                            date_time_str = article.find('div', class_='c-byline')['data-cdata']
+                            date_time_json = json.loads(date_time_str)
+                            date_time = datetime.utcfromtimestamp(int(date_time_json['timestamp']))
 
-            # Pause for a short time to avoid overwhelming the server
-            time.sleep(1)
+                            headline = article.find('h2', class_='c-entry-box--compact__title').find('a').text
+                            link = article.find('h2', class_='c-entry-box--compact__title').find('a')['href']
 
-        else:
-            print(f"No news table found for {category} on page {page_number}")
-            break
+                            all_data.append([date_time, headline, link])
+                            print(date_time, headline, link)
+                    else:
+                        print(f"No news table found for {category} on {year}-{month}-{day}")
 
-    # Create the 'df' DataFrame
-    df = pd.DataFrame(all_data, columns=['date_val', 'headline', 'link'])
+                    # Pause for a short time to avoid overwhelming the server
+                    time.sleep(1)
+
+                except HTTPError as e:
+                    print(f"Error for {category} on {year}-{month}-{day}: {e}")
+
+                # Pause before moving to the next day
+                time.sleep(1)
+
+    # Create the 'df' DataFrame outside the loop
+    df = pd.DataFrame(all_data, columns=['date_time', 'headline', 'link'])
 
     # Remove duplicate headlines
     df = df.drop_duplicates(subset=['headline'])
 
-    return df
+    # Sort the DataFrame by 'date_val' in descending order
+    df = df.sort_values(by='date_time', ascending=False)
 
-category = 'tech'
-start_date = datetime(2022, 1, 1)
-news_df = get_news_sentiment(category, start_date)
-print(news_df)
+    all_data.append(df)
+
+    return df
